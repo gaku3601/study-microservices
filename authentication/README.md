817f4fddeaecde755c6fb79b6c10bfd0b39ca165
gaku0601 <pro.gaku@gmail.com>
2017-11-13 08:52:16 +0900

    擬似認証用のapiを作成

---
+++ authentication/README.md
@@ -0,0 +1,5 @@
+# 認証用のcurl
+
+    curl -H 'Content-Type:application/json' -H 'User-Agent:iPhone' -H 'Accept-Encoding:gzip,deflate' -d '{"ID":"gaku","pass":"gakugaku"}' http://localhost:8080/users/auth
+
+
--- authentication/test.go
+++ authentication/test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	mux "github.com/gorilla/mux.git"
@@ -10,10 +11,34 @@ func main() {
 	r := mux.NewRouter()
 	// 単純なハンドラ
 	r.HandleFunc("/", YourHandler).Methods("POST")
+	r.HandleFunc("/users/auth", UserAuth).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
 
+type User struct {
+	ID     string `json:"id"`
+	Pass   string `json:"pass"`
+	IsAuth bool   `json:"isauth"`
+}
+
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Gorillaaaaaaaaadaaaaaaaa!\n"))
+}
+
+func UserAuth(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	error := decoder.Decode(&user)
+	if error != nil {
+		w.Write([]byte("json decode error " + error.Error() + "\n"))
+	}
+
+	//認証処理
+	if user.ID == "gaku" && user.Pass == "gakugaku" {
+		user.IsAuth = true
+	} else {
+		user.IsAuth = false
+	}
+	json.NewEncoder(w).Encode(user)
 }
