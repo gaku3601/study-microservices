b8d3a6f6301e57b0ee456317e68b8dd9571a05fc
gaku0601 <pro.gaku@gmail.com>
2017-11-16 08:38:43 +0900

    dbへのuser登録処理を実装

--- authentication/main.go
+++ authentication/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 
 	mux "github.com/gorilla/mux.git"
 	viper "github.com/spf13/viper.git"
@@ -17,13 +20,14 @@ func main() {
 	// 単純なハンドラ
 	r.HandleFunc("/", YourHandler).Methods("POST")
 	r.HandleFunc("/users/login", Login).Methods("POST")
+	r.HandleFunc("/users/signup", SignUp).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
 
 type User struct {
-	ID   string `json:"id"`
-	Pass string `json:"pass"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type Response struct {
@@ -45,7 +49,7 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	//認証処理
 	res := new(Response)
-	if user.ID == "gaku" && user.Pass == "gakugaku" {
+	if user.Email == "gaku" && user.Password == "gakugaku" {
 		//jwtトークンの取得
 		res.Token = fetchCreateToken()
 	} else {
@@ -57,6 +61,24 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 //ユーザ登録
 func SignUp(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	user := new(User)
+
+	error := decoder.Decode(&user)
+	if error != nil {
+		w.Write([]byte("json decode error " + error.Error() + "\n"))
+	}
+
+	//DB登録
+	db, _ := sql.Open("postgres", "user=postgres host=localhost dbname=auth_db port=5433 sslmode=disable")
+	defer db.Close()
+
+	_, err := db.Exec("INSERT INTO users(email, password) VALUES($1, $2);", user.Email, user.Password)
+	if err != nil {
+		w.Write([]byte("Signup DB insert error: " + err.Error() + "\n"))
+	} else {
+		w.Write([]byte("Signup OK\n"))
+	}
 }
 
 //configファイルの読み込み
