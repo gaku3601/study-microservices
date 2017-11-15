3f39123a7bfb17f92097e24e94396f3a30fef90d
gaku0601 <pro.gaku@gmail.com>
2017-11-15 20:14:08 +0900

    auth用のDBを追加し、databaseが無い場合、自動的に作成するように実装

--- authentication/build.sh
+++ authentication/build.sh
@@ -1,5 +1,12 @@
 cd /go/src/authentication
 go build .
 
+if ! psql -q -U postgres -h auth-db -lqt | cut -d \| -f 1 | grep -wq auth_db; then
+  echo "Databaseが存在しません。作成を行います。"
+  psql -q -U postgres -h auth-db -c "create database auth_db;"
+fi
+
+echo "Database OK"
+
 #起動
 ./authentication
