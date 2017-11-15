85433908165a546d2bb0670f01eea2998de24719
gaku0601 <pro.gaku@gmail.com>
2017-11-16 07:58:39 +0900

    gooseでuserテーブルを作成

---
+++ authentication/db/migrations/20171116074412_CreateTableUsers.sql
@@ -0,0 +1,13 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+create table Users (
+  id            SERIAL     primary key,
+  email char(40),
+  password char(60)
+);
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+drop table Users;
