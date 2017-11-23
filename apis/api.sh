3376532dea28f7dc3f4a3454f6b6bc58a07c3723
gaku0601 <pro.gaku@gmail.com>
2017-11-23 18:39:03 +0900

    db作成をgolangAppで行なっていたが、廃止しpostgreコンテナの環境変数で行うよう変更。また、起動時にuserを作成するapiを追加

--- apis/api.sh
+++ apis/api.sh
@@ -6,6 +6,9 @@ do
     else
       echo "starting confirm! Adding api!"
       curl -i -X POST --url http://kong:8001/apis/ --data 'name=google' --data 'uris=/google' --data 'upstream_url=https://www.google.co.jp' --data 'methods=GET'
+
+      #jwt認証用ユーザ
+      curl -X POST http://kong:8001/consumers     --data "username=gaku"     --data "custom_id=gaku"
       break
     fi
     sleep 5;
