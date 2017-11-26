2ed6043c510962f5c7998ad7264a53537f080ee4
gaku0601 <pro.gaku@gmail.com>
2017-11-27 08:45:28 +0900

    kong経由でAPIを実行できるように設定

--- apis/api.sh
+++ apis/api.sh
@@ -5,10 +5,22 @@ do
       echo "start waiting..."
     else
       echo "starting confirm! Adding api!"
+      #test用google
       curl -i -X POST --url http://kong:8001/apis/ --data 'name=google' --data 'uris=/google' --data 'upstream_url=https://www.google.co.jp' --data 'methods=GET'
 
       #jwt認証用ユーザ
       curl -X POST http://kong:8001/consumers     --data "username=gaku"     --data "custom_id=gaku"
+
+      #ユーザ登録
+      curl -i -X POST --url http://kong:8001/apis/ --data 'name=signup' --data 'uris=/users/signup' --data 'upstream_url=http://auth:8080/users/signup' --data 'methods=POST'
+      #ログイン
+      curl -i -X POST --url http://kong:8001/apis/ --data 'name=login' --data 'uris=/users/login' --data 'upstream_url=http://auth:8080/users/login' --data 'methods=POST'
+
+
+
+      #パスワード変更
+      curl -i -X POST --url http://kong:8001/apis/ --data 'name=changepw' --data 'uris=/change_password' --data 'upstream_url=http://auth:8080/users/change_password' --data 'methods=POST'
+      curl -X POST http://kong:8001/apis/changepw/plugins     --data "name=jwt"
       break
     fi
     sleep 5;
--- authentication/build.sh
+++ authentication/build.sh
@@ -1,5 +1,9 @@
-cd /go/src/authentication
+cd /go/src/github.com/gaku3601/study-microservices/authentication
+
+#DBセットアップ
+goose -env production up
 
 #ビルド&起動
 go build .
 ./authentication
+#while sleep 3600; do :; done
